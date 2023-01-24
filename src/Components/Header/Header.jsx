638676f21f269493748e629679681f1344385ab6
tonmoy5 <tonmoy52532@gmail.com>
2023-01-24 14:42:58 +0600

    Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './Header.css'
+import img from '../../Media/ImageHeaderPhoto.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="header">
+      <div className="wrapper">
+
+
+        <div className="left">
+          <div className="top">
+            <div className="text">DONATE</div>
+          </div>
+
+          <div className="bottom">
+            <h1 className="heading">
+              Making a donation for Nature.
+            </h1>
+            <p className="paragraph">
+              When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet.
+            </p>
+            <button className="btn">
+              Donate now
+            </button>
+          </div>
+        </div>
+
+
+        <div className="right">
+          <img src={img} alt="" className='photo' />
+        </div>
+      </div>
+    </section>
   )
 }
 
