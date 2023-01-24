ee1c97e4fcdf1cb262dba6acc351bcfba23c22f9
tonmoy5 <tonmoy52532@gmail.com>
2023-01-24 16:18:51 +0600

    Use Section

--- src/Components/Usage/Usage.jsx
+++ src/Components/Usage/Usage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Usage.css'
 
 const Usage = () => {
   return (
-    <div>Usage</div>
+    <section className="use">
+      <div className="wrapper">
+        <div className="left">
+          <h1 className="useTitle">
+            How we use your donation
+          </h1>
+        </div>
+        <div className="center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.  Nunc ut sem vitae risus tristique posuere.
+        </div>
+        <div className="right">
+          Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.
+        </div>
+      </div>
+    </section>
   )
 }
 
