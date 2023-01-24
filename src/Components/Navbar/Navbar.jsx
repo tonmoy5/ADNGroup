a0d5ad1287db94bfa3a5a694c7a0ada28d94b332
tonmoy5 <tonmoy52532@gmail.com>
2023-01-24 13:55:52 +0600

    Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="wrapper">
+        <div className="navbar-left">
+          <Link to="/" className="navbar-logo">{"{"}Finsweet</Link>
+        </div>
+        <div className="navbar-right">
+          <ul className="navbar-links">
+            <li className='active'>
+              <Link to="/about">Home</Link>
+            </li>
+            <li>
+              <Link to="/services">About Us</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">What We Do</Link>
+            </li>
+            <li>
+              <Link to="/contact">Media</Link>
+            </li>
+            <li>
+              <Link to="/blog">Contact</Link>
+            </li>
+            <li>
+              <button className="navbar-btn">Donate</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
